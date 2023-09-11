f1727e87e11f38a12e070ec56bac4fb568d967d7
Anuanushree <anuabi2408@gmail.com>
2023-09-11 22:04:27 +0530

    hall-booking

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(config.MONGO_DB)
-   .then(()=>{
-    console.log("Connected to the mongodb");
-    app.listen(config.PORT,()=>{
-        console.log("Server connecting Port :" ,config.PORT)
+    .then(() => {
+        console.log("Connected to the mongodb");
+        app.listen(config.PORT, () => {
+            console.log("Server connecting Port :", config.PORT)
+        })
     })
-   })
-   .catch((error)=>{
-    console.log("Error in connecting mongodb: " ,error)
-   })
+    .catch((error) => {
+        console.log("Error in connecting mongodb: ", error)
+    })
